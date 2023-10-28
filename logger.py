cb8a82fb7fedc2ee54ff24bbad9bb095445874d5
Neito3350 <123307134+Neito3350@users.noreply.github.com>
2023-10-29 00:34:42 +0600

    add "no_log_file" attribute

--- logger.py
+++ logger.py
@@ -2,22 +2,21 @@ from time import gmtime, strftime
 
 class Logger():
 
-    def __init__(self) -> None:
-        self._log_list = []
+    def __init__(self, no_log_file:bool=True) -> None:
+        self.__no_log_file = no_log_file
         self._LOG_INFO = "[INFO]"
         self._LOG_ERROR = "[ERROR]"
 
-    def _log(self, log_type, message):
+    def __log(self, log_type, message):
         timeNow = strftime("%H:%M:%S", gmtime())
-        self._log_list.append(f"[{timeNow}]{log_type}: {message};")
-
-        with open("log.txt", "a+", encoding="utf-8") as logfile:
-            logfile.write(f"[{timeNow}]{log_type}: {message};\n")
-            
         print(f"[{timeNow}]{log_type}: {message};")
 
+        if not self.__no_log_file:
+            with open("log.txt", "a+", encoding="utf-8") as logfile:
+                logfile.write(f"[{timeNow}]{log_type}: {message};\n")
+            
     def log_info(self, message):
-        self._log(self._LOG_INFO, message)
+        self.__log(self._LOG_INFO, message)
 
     def log_error(self, message):
-        self._log(self._LOG_ERROR, message)
+        self.__log(self._LOG_ERROR, message)
